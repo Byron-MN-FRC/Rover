8379266cb703e795cd2b6ae8ecbddfc44da11650
bdilwo50 <bdilwo50@students.byron.k12.mn.us>
2019-01-19 19:07:00 -0600

    Button Mapping Fixed

--- src/main/java/org/usfirst/frc4859/Rover/commands/ShiftGravity.java
+++ src/main/java/org/usfirst/frc4859/Rover/commands/ShiftGravity.java
@@ -37,6 +37,7 @@ public class ShiftGravity extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        Robot.climb.shiftGravity();
     }
 
     // Called repeatedly when this Command is scheduled to run
