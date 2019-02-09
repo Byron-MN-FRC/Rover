b247cca346a399be6ec61d45e045c6246958b915
ilyasali12 <iali30@students.byron.k12.mn.us>
2019-02-08 18:10:34 -0600

    made changes with subsystems (i.e. fire = firecatapult)

--- src/main/java/org/usfirst/frc4859/Rover/Constants.java
+++ src/main/java/org/usfirst/frc4859/Rover/Constants.java
@@ -4,6 +4,8 @@
 
 package org.usfirst.frc4859.Rover;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 //import org.usfirst.frc4859.Rover.Gains;
 
 public class Constants {
@@ -30,5 +32,12 @@ public class Constants {
 	 * Gains used in Motion Magic, to be adjusted accordingly
      * Gains(kp, ki, kd, kf, izone, peak output);
      */
-    public static final Gains kGains = new Gains(0.2, 0.0, 0.0, 0.2, 0, 1.0);
+	public static final Gains kGains = new Gains(0.2, 0.0, 0.0, 0.2, 0, 1.0);
+	
+	public static TalonSRX kFeetMotor;
+
+	public static final double kUpSpeed = .45;
+	public static final double kDownSpeed = .15; 
+	public static final double kUpTime = .45;
+	public static final double kDownTime = .5;
 }
